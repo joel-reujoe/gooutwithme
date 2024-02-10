1ba40bf2bb7a4b40a4ae305707ebe2a9a3358722
fp-joel-dsouza <joel.dsouza@financepeer.co>
2024-02-10 15:12:49 +0530

    added express server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 const path = require('path')
-app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 
 
 let port = process.env.PORT || 8000;
